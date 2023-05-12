586162e00db2dc169747d646fee688e6b75f4a1e
Octávio Marques <octavio12@live.com.pt>
2023-05-12 15:17:25 +0100

    Fixed Resources panel
    resources panel now is not at the house door;
    Resources Panel allways shows and works;

--- scenes/player/script/player.gd
+++ scenes/player/script/player.gd
@@ -7,7 +7,21 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var player_class; # Player Class File:
+
+# Resources:
+var uncut_saphires = 0;
+var uncut_emeralds = 0;
+var cut_saphires = 0;
+var cut_emeralds = 0;
+var wood_logs = 0;
+var wood_planks = 0;
+var gold_coins = 0;
+
 func _ready():
+	# Reads Player Class:
+	player_class = get_node("/root/Player");
+	update_resources();
 	$emerald_information_msg.hide();
 	$saphire_information_msg.hide();
 
@@ -29,3 +43,27 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	update_resources();
+
+# Function used to update game resources with file resources:
+func update_resources():
+	var player = player_class.get_player();
+	uncut_saphires = player["uncut_saphires"];
+	uncut_emeralds = player["uncut_emeralds"];
+	cut_saphires = player["cut_saphires"];
+	cut_emeralds = player["cut_emeralds"];
+	wood_logs = player["wood_logs"];
+	wood_planks = player["wood_planks"];
+	gold_coins = player["gold_coins"];
+	update_resources_labels();
+
+# Function used to update resources labels:
+func update_resources_labels():
+	$resources_window/uncut_saphire_label.text = str(uncut_saphires);
+	$resources_window/uncut_emerald_label.text = str(uncut_emeralds);
+	# TODO: Not Implemented:
+	#$resources_window.get_node("resources_container").get_node("cut_saphire_label").text = str(cut_saphires);
+	#$resources_window.get_node("resources_container").get_node("cut_emerald_label").text = str(cut_emeralds);
+	#$resources_window.get_node("resources_container").get_node("wood_logs_label").text = str(wood_logs);
+	#$resources_window.get_node("resources_container").get_node("wood_planks_label").text = str(wood_planks);
+	#$resources_window.get_node("resources_container").get_node("gold_coins_label").text = str(gold_coins);
